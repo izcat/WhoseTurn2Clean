fbc27f6e83522a123077ef04d386a4e47721b152
izcat <de.ep@qq.com>
2021-10-08 22:45:02 +0800

    增加邮箱

--- emailsInfo.py
+++ emailsInfo.py
@@ -1,15 +1,15 @@
 
-allInfo = ( {'id':1, 'name':'熊晓辉', 'email':''}, 
-            {'id':2, 'name':'何梓科', 'email':''}, 
-            {'id':3, 'name':'王唐胜', 'email':''}, 
+allInfo = ( {'id':1, 'name':'熊晓辉', 'email':'1948492662@qq.com'}, 
+            {'id':2, 'name':'何梓科', 'email':'2826640380@qq.com'}, 
+            {'id':3, 'name':'王唐胜', 'email':'1476390673@qq.com'}, 
             {'id':4, 'name':'宗永承', 'email':'yc.zong@siat.ac.cn'}, 
             {'id':5, 'name':'郑博匀', 'email':'564420559@qq.com'}, 
             {'id':6, 'name':'赵子昊', 'email':'1194148199@qq.com'}, 
-            {'id':7, 'name':'胡睿', 'email':''}, 
-            {'id':8, 'name':'李梓聖', 'email':''}, 
-            {'id':9, 'name':'何致涛', 'email':''}, 
-            {'id':11, 'name':'周威', 'email':''}, 
-            {'id':12, 'name':'陈文雄', 'email':''}, 
+            {'id':7, 'name':'胡睿', 'email':'1059696078@qq.com'}, 
+            {'id':8, 'name':'李梓聖', 'email':'928395118@qq.com'}, 
+            {'id':9, 'name':'何致涛', 'email':'syne31415@163.com'}, 
+            {'id':11, 'name':'周威', 'email':'1114700322@qq.com'}, 
+            {'id':12, 'name':'陈文雄', 'email':'1528795407@qq.com'}, 
             {'id':13, 'name':'许清林', 'email':'122176112@qq.com'} )
 
 
