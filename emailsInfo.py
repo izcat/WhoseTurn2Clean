33a220cb89726d0356d54bff32e5419352836a50
izcat <de.ep@qq.com>
2021-10-08 22:16:16 +0800

    增加邮箱

--- emailsInfo.py
+++ emailsInfo.py
@@ -4,13 +4,13 @@ allInfo = ( {'id':1, 'name':'熊晓辉', 'email':''},
             {'id':3, 'name':'王唐胜', 'email':''}, 
             {'id':4, 'name':'宗永承', 'email':'yc.zong@siat.ac.cn'}, 
             {'id':5, 'name':'郑博匀', 'email':'564420559@qq.com'}, 
-            {'id':6, 'name':'赵子昊', 'email':''}, 
+            {'id':6, 'name':'赵子昊', 'email':'1194148199@qq.com'}, 
             {'id':7, 'name':'胡睿', 'email':''}, 
             {'id':8, 'name':'李梓聖', 'email':''}, 
             {'id':9, 'name':'何致涛', 'email':''}, 
             {'id':11, 'name':'周威', 'email':''}, 
             {'id':12, 'name':'陈文雄', 'email':''}, 
-            {'id':13, 'name':'许清林', 'email':''} )
+            {'id':13, 'name':'许清林', 'email':'122176112@qq.com'} )
 
 
 # 昨天谁值日
